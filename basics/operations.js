8047ba6fe73541a88fa687ac1e03b4e502cd9d2f
Parth Srivastava <83111106+ParthSrivastava500@users.noreply.github.com>
2024-03-07 07:06:41 +0000

    commenced

---
+++ basics/operations.js
@@ -0,0 +1,16 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2) // 2 ki power 2
+console.log(2/1)
+console.log(2%1) // for remainder
+let str1 = "string1"
+let str2 = "string2"
+let str3 = str1+str2
+console.log(str3)
+//--------------------some tricky operations----------------------------------------
+console.log("1"+ 2+2) //agar string aur number ko milaoge toh agar string pehle likha h toh side me number lag jaayega jaise string add hoti h
+console.log(2+2+"1") //agar number aur string ko mila rhe ho aur number pehle likhe h toh woh add ho jaayenge aur string chipak jaayegi baad me 
